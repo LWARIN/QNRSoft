8f9fc5191263d0d23e514600e8faa9809acd7381
Laurent Warin <l.warin@sfr.fr>
2013-04-19 14:43:47 +0200

    #6 Test unitaire Answer

--- test/unit/qnrsoft/AnswerTests.groovy
+++ test/unit/qnrsoft/AnswerTests.groovy
@@ -11,27 +11,30 @@ import org.junit.*
 @TestFor(Answer)
 class AnswerTests {
 
-    void testConstraints() {
-       Answer ans = new Answer()
+	void testConstraints() {
+		Answer ans = new Answer()
 
 		assert !ans.validate()
 		assert "nullable" == ans.errors["answer"].code
 
 		ans = new Answer(answer:"4",
-			status:"Status",
-			validity:"Wrong",
-			comment:"",
-			voteCount:0)
+		status:"Status",
+		validity:"Wrong",
+		comment:"",
+		voteCount:0)
 
 		assert !ans.validate()
 		assert "not.inList" == ans.errors["status"].code
 
-		ans = new Answer(answer : "Je dirais que ca fait 4",
-			status : Answer.STATUS_APPROVED, validity : Answer.VALIDITY_CORRECT,
-			comment : "salut", voteCount: 0)
-		
-		// ?
+		Quizz q = new Quizz(question:"2+2?",
+		onScreen:false,
+		state:"Opened",
+		voteCount:0)
 
-		assert ans.validate()
-    }
+		ans = new Answer(answer : "Je dirais que ca fait 4",
+		status : Answer.STATUS_APPROVED, validity : Answer.VALIDITY_CORRECT,
+		comment : "salut", voteCount: 0, quizz: q)
+
+		ans.validate()
+	}
 }
